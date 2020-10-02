a7ad5a2eb6fa2ea7061a0eed55762659759d7996
leandrodss300 <leandrodss300@gmail.com>
2020-10-01 21:48:25 -0300

    01/10/20

--- CALCULADORA/CalculadoraController.js
+++ CALCULADORA/CalculadoraController.js
@@ -2,9 +2,19 @@ class CalculadoraController{
     //Método que é executado quando um objeto é instanciado
     constructor(){
         //Selecionar os botões
-        this.buttons = document.querySelectorAll("#buttons > g")
+        this.buttons = document.querySelectorAll("#buttons > g, parts > g")
         this.listaComandos = []
         this._display = document.querySelector("#display")
+        let dataElement = document.querySelector('#data')
+        let date = new Date();
+        dataElement.innerHTML = date.toLocaleDateString('pt-BR');
+
+        var horaElement = document.querySelector('#hora')
+        setInterval(function(){
+            horaElement.innerHTML = new Date().toLocaleTimeString('pt-BR')
+        },1000)
+
+        horaElement.innerHTML = date.toLocaleTimeString('pt-BR')
 
         this.inicializarButtons()
     }
@@ -25,6 +35,9 @@ class CalculadoraController{
     }
     selecionaButton(nomeBotao){
         switch(nomeBotao){
+            case "0":
+                this.adicionarNumero(nomeBotao)
+                break;
             case "1":
                 this.adicionarNumero(nomeBotao)
                 break;
@@ -55,27 +68,46 @@ class CalculadoraController{
             case "soma":
                 this.adicionarOperacao('+')
                 break;
-            case "soma":
-                this.adicionarOperacao('+')
+            case "multiplicacao":
+                this.adicionarOperacao('*')
                 break;
-            case "soma":
-                this.adicionarOperacao('+')
+            case "divisao":
+                this.adicionarOperacao('/')
                 break;
-            case "soma":
-                this.adicionarOperacao('+')
+            case "subtracao":
+                this.adicionarOperacao('-')
                 break;
             case "igual":
                 this.calcular()
+                break;
+            case "ponto":
+                this.adicionarPonto(".")
+                break;
+            case "ac":
+                this.apagarTudo()
+                break;
+            case "ce":
+                this.apagarCe()
+                break;
+            case "porcento":
+                this.adicionarOperacao("%")
+                
                 break;
         }
         console.log(this.listaComandos)
     }
     calcular(){
         //Transforma lista em string
-       let expressao = this.listaComandos.join("")
-       let result = eval(expressao)
-       this.display = result
-       this.atribuirResultado(result)
+        if(this.listaComandos[1] == "%"){
+            let valor = this.listaComandos[0]*this.listaComandos[2]/100
+            this.listaComandos = valor
+            this.display = valor
+        }else{
+            let expressao = this.listaComandos.join("")
+            let result = eval(expressao)
+            this.display = result
+            this.atribuirResultado(result)
+        }
 
     }
     adicionarNumero(num){
@@ -89,8 +121,53 @@ class CalculadoraController{
         }
         this.display = this.ultimoComando
     }
+    adicionarPonto(po){
+        //Se o último cmd não for um número
+        if( isNaN(this.ultimoComando) ){
+            if(this.ultimoComando = "."){
+                let ponto = this.ultimoComando.toString() + po.toString()
+                this.ultimoComando =ponto
+            }else{
+                this.listaComandos.push(num)
+            }
+        }else{
+            //É um número -> concatenar
+            let novoValor = this.ultimoComando.toString() + po.toString()
+            this.ultimoComando = novoValor
+        }
+        this.display = this.ultimoComando
+    }
+    apagarTudo(){
+        this.listaComandos = this.listaComandos = []
+        this.display = "0"
+    }
+    apagarCe(){
+        //let ce = this.ultimoComando
+        //this.listaComandos = this.listaComandos.splice(0,this.listaComandos.length - 1)
+        console.log(this.listaComandos)
+        if(this.listaComandos.length <= 1){
+            this.display = "0"
+            this.listaComandos = []
+        }else if(this.listaComandos.length === 2){
+            this.listaComandos = this.listaComandos.splice(0,this.listaComandos.length - 1)
+            this.display = this.ultimoComando
+        }
+        //this.display = this.ultimoComando
+        /*
+        if( isNaN(this.ultimoComando) ){
+            console.log("ce")
+            this.listaComandos = this.listaComandos.splice(0,this.listaComandos.length - 1)
+            this.display = this.ultimoComando 
+        }else if(this.listaComandos.length==0){
+            this.display = "0"
+            return;
+        }*/
+    }
     adicionarOperacao(op){
         this.listaComandos.push(op)
+    }
+    primeiroValor(){
+        this.listaComandos.indexOf(0)
     }
     get ultimoComando(){
         return this.listaComandos[ this.listaComandos.length - 1 ]
@@ -102,10 +179,12 @@ class CalculadoraController{
         this.listaComandos = [valor]
     }
     get display(){
-        return this._display.innerHTML
+        return this._display.innerHTML  
     }
     set display(valor){
         this._display.innerHTML = valor
     }
 
 }
+//Audio.play()
+//let audio = new Audio("click.mp3")
